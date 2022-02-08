18b3fdafa67e2f8f13f1a8f1b79d0721c78b7e6d
AthaliaMabalon <99074489+AthaliaMabalon@users.noreply.github.com>
2022-02-08 03:16:03 -0500

    Update cle.php

--- cle.php
+++ cle.php
@@ -5,3 +5,5 @@
 <body background="cle.jpg" style="background-size: 100%;">
 
 	<h1> <center> <br><br> <b> <u> <font face="Book Antiqua" size="30"> 	CLE </b> </u> </font> </center>
+		</body>
+	</html>
